ef6941337b58c587232659cb8e692ae5d6fe9c10
Shao Jie <tayshaojie@gmail.com>
2022-01-09 22:59:04 +0800

    part 2

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
+const generateImage = require("./generateImage")
+
 const streamPASS = process.env.streamPASS
 
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        // "GUILD_MEMBERS"
     ]
 })
 
@@ -15,13 +18,25 @@ client.on("ready", () => {
 })
 
 client.on("messageCreate", (message) => {
-    if (message.content == "hi"){
-        message.reply("Hello World!")
-    }
+    // if (message.content == "hi"){
+    //     message.reply("Hello World!")
+    // }
 
     if (message.content == "t!pw"){
         message.reply("The password is " + streamPASS)
     }
+
+
+})
+
+// Adding Welcome Message to the server
+const welcomeChannelID = "929741941606875146"
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelID).send({
+        content: `<@${member.id}> Welcome to the server!`, 
+        files: [img]
+    })
 })
 
 client.login(process.env.TOKEN)
